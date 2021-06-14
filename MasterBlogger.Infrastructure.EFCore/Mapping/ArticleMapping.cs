47eda4c67b2eee53856ce3957c1d00be882f08f7
MrHamid-am <70596796+MrHamid-am@users.noreply.github.com>
2021-06-14 15:17:53 +0430

    Implement Article Domain & Mappings - Create Application & Repository Structure

--- MasterBlogger.Infrastructure.EFCore/Mapping/ArticleMapping.cs
+++ MasterBlogger.Infrastructure.EFCore/Mapping/ArticleMapping.cs
@@ -4,26 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MB.Domain.ArticleAgg;
-using MB.Domain.ArticleCategoryAgg;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace MB.Infrastructure.EFCore.Mapping
 {
-    public class ArticleCategoryMapping:IEntityTypeConfiguration<ArticleCategory>
+    public class ArticleMapping : IEntityTypeConfiguration<Article>
     {
-        public void Configure(EntityTypeBuilder<ArticleCategory> builder)
+        public void Configure(EntityTypeBuilder<Article> builder)
         {
-            builder.ToTable("ArticleCategories");
+            builder.ToTable("Articles");
 
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Title);
+            builder.Property(x => x.ShortDescription);
+            builder.Property(x => x.Image);
+            builder.Property(x => x.Content);
             builder.Property(x => x.CreationDate);
             builder.Property(x => x.IsDeleted);
 
-            builder.HasMany(x=>x.Articles)
-                .WithOne(x => x.ArticleCategory)
-                .HasForeignKey(x=>x.ArticleCategoryId);
+            builder.HasOne(x => x.ArticleCategory)
+                .WithMany(x => x.Articles)
+                .HasForeignKey(x => x.ArticleCategoryId);
         }
     }
 }
