7558d5507ca88aa564098ebcee1a89ebec028d4c
MrHamid-am <70596796+MrHamid-am@users.noreply.github.com>
2021-06-16 15:58:30 +0430

    Implement Edit article

--- MasterBlogger.Application.Contracts/Article/IArticleApplication.cs
+++ MasterBlogger.Application.Contracts/Article/IArticleApplication.cs
@@ -9,7 +9,8 @@ namespace MB.Application.Contracts.Article
     public interface IArticleApplication
     {
         List<ArticleViewModel> GetList();
-
         void Create(CreateArticle command);
+        void Edit(EditArticle command);
+        EditArticle Get(long id);
     }
 }
