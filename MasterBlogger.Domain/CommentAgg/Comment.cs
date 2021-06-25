83d6bf2a168c99efc6fffec03d552cb1d452608a
MrHamid-am <70596796+MrHamid-am@users.noreply.github.com>
2021-06-25 15:37:14 +0430

    Implement Confirm & Cancel actions in comment management

--- MasterBlogger.Domain/CommentAgg/Comment.cs
+++ MasterBlogger.Domain/CommentAgg/Comment.cs
@@ -11,9 +11,9 @@ namespace MB.Domain.CommentAgg
     {
         public long Id { get; private set; }
         public string Name { get; private set; }
-        public string Email { get; private set; }   
+        public string Email { get; private set; }
         public string Message { get; private set; }
-        public int Status { get; private set; }// 0: new, 1: confirmed, 2:cancelled
+        public int Status { get; private set; }
         public DateTime CreationDate { get; private set; }
         public long ArticleId { get; private set; }
         public Article Article { get; set; }
@@ -30,7 +30,17 @@ namespace MB.Domain.CommentAgg
 
         protected Comment()
         {
-            
+
+        }
+
+        public void Confirm()
+        {
+            Status = Statuses.Confirm;
+        }
+
+        public void Cancel()
+        {
+            Status = Statuses.Cancelled;
         }
     }
 }
--- MasterBlogger.Domain/CommentAgg/ICommentRepository.cs
+++ MasterBlogger.Domain/CommentAgg/ICommentRepository.cs
@@ -11,5 +11,7 @@ namespace MB.Domain.CommentAgg
     {
         void CreateAndSave(Comment entity);
         List<CommentViewModel> GetList();
+        Comment Get(long id);
+        void Save();
     }
 }
